fbb350b1104999c90ba10b3fc5cbc356624742ad
Jack <jacklshanks@gmail.com>
2024-11-18 16:08:05 +0000

    feat!: Changed main.py to functional run

--- Bot/main.py
+++ Bot/main.py
@@ -1,19 +1,28 @@
 import discord
+import os
 from ..Config.ConfigLoader import Config
 from discord.ext import commands
 from Cogs.QuoteManager import QuoteManager
 from Cogs.AssignColour import AssignColour
 
-BOT_TOKEN = ''
-
 intents = discord.Intents.all()
-
 bot = commands.Bot(intents=intents)
+
 
 @bot.event
 async def on_ready():
     bot.add_cog(QuoteManager(bot))
     bot.add_cog(AssignColour(bot))
 
+
 Config.LoadConfig()
-bot.run(BOT_TOKEN)
+
+# Uses environmental variable to get token then displays errors upon bot run
+try:
+    token = os.getenv("auth")
+    bot.run(token)
+except discord.HTTPException as e:
+    if e.status == 429:
+        print("The Discord servers denied the connection for making too many requests")
+    else:
+        raise e
