cb3636d588342679c0e2f4929458fe44e8e35485
jackshanks <jacklshanks@gmail.com>
2024-11-19 10:18:47 +0000

    fix(database): Added some comments

--- Database/DatabaseHandler.py
+++ Database/DatabaseHandler.py
@@ -26,6 +26,7 @@ class DatabaseHandler:
             print("Database connection closed")
 
     async def execute_query(self, query: str, *args):
+        """Execute a database query."""
 
         if not self._connection:
             raise Exception("Database connection not initialized")
@@ -39,11 +40,13 @@ class DatabaseHandler:
             raise
 
     async def get_quote(self):
+        """Get a random quote."""
         quote_number = await self.get_number_of_quotes()
         quote_id = random.randint(1, quote_number)
         result = await self.execute_query("SELECT * FROM quotes WHERE id = ?", quote_id)
         return result
 
     async def get_number_of_quotes(self):
+        """Get number of quotes in the quote table."""
         result = await self.execute_query("SELECT COUNT(*) FROM quotes")
         return result[0]
