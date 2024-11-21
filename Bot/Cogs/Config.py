2c9e0b2854d4dcdfa8d440f7acb74b244195a05f
Jamie Atkinson <142546744+JamieLAtkinson@users.noreply.github.com>
2024-11-21 15:14:45 +0000

    fix config

--- Bot/Cogs/Config.py
+++ Bot/Cogs/Config.py
@@ -11,6 +11,6 @@ class ConfigCommands(BaseCog):
     @nextcord.slash_command(name="reloadconfig", description="reloads the config", guild_ids=Config().guild_ids)
     @application_checks.has_permissions(administrator=True)
     async def reloadconfig(self, ctx: Interaction):
-        Config().config.LoadConfig()
+        Config.LoadConfig()
         print(f"Config reloaded at {datetime.datetime.now()} by {ctx.user.name}({ctx.user.id})")
         await ctx.response.send_message("done", ephemeral=True)
