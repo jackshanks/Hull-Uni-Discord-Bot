a6731443eda6ceb1a1b2f152a2fc74f095512f95
jackshanks <jacklshanks@gmail.com>
2024-12-02 18:45:06 +0000

    fix(Music): Started to try and make the music commands less messy

---
+++ Bot/Cogs/Managers/MusicManager.py
@@ -0,0 +1,18 @@
+﻿import nextcord
+import nextcordwavelink as wavelink
+
+async def create_track_embed(track: wavelink.Playable, original) -> nextcord.Embed:
+    """Helper method to create track information embed."""
+    embed: nextcord.Embed = nextcord.Embed(title="Now Playing")
+    embed.description = f"**{track.title}** by `{track.author}`"
+
+    if track.artwork:
+        embed.set_image(url=track.artwork)
+
+    if original and original.recommended:
+        embed.description += f"\n\n`This track was recommended via {track.source}`"
+
+    if track.album.name:
+        embed.add_field(name="Album", value=track.album.name)
+
+    return embed
